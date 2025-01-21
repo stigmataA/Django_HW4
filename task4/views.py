525758325e3bcbd746c52891bf082dc1a1c42965
stigmataA <r.muhutdinov@gmail.com>
2025-01-21 16:28:18 +0300

    Update views.py

--- task4/views.py
+++ task4/views.py
@@ -3,15 +3,46 @@ from django.shortcuts import render
 
 # Create your views here.
 def platform(request):
-    return render(request, 'fourth_task/platform.html')
+    title = 'Главная страница'
+    menu_choice = {'main': 'Главная', 'shop': 'Магазин', 'cart': 'Корзина'}
+    context = {
+        'title': title,
+        'menu_choice': menu_choice,
+    }
+    return render(request, 'fourth_task/platform.html', context)
 
 
 def games(request):
-    context = {'games': ['Atomic Heart', 'Cyberpunk 2077', 'PayDay 2']}
+    title = 'Магазин'
+    text = 'Игры'
+    buy = 'Купить'
+    back = 'Вернуться обратно'
+    games_dict = {'games': ["Atomic Heart", "Cyberpunk 2077", "Pay Day 2"]}
+    menu_choice = {'main': 'Главная', 'shop': 'Магазин', 'cart': 'Корзина'}
+    context = {
+        'title': title,
+        'text': text,
+        'buy': buy,
+        'back': back,
+        'games_dict': games_dict,
+        'menu_choice': menu_choice,
+    }
     return render(request, 'fourth_task/games.html', context)
 
 
 def cart(request):
-    return render(request, 'fourth_task/cart.html')
+    title = 'Корзина'
+    message = "Извините, Ваша корзина пуста"
+    back = 'Вернуться на главную'
+    menu_choice = {'main': 'Главная', 'shop': 'Магазин', 'cart': 'Корзина'}
+    context = {
+        'title': title,
+        'message': message,
+        'back': back,
+        'menu_choice': menu_choice,
+    }
+    return render(request, 'fourth_task/cart.html', context)
+
+
 
 
